3fbebdfcb8c7649a5113b91bff990c6dd8a37681
Marlon Petry <marlonpetry@gmail.com>
2023-05-24 19:05:11 -0300

    noVNC Update

--- vnc_startup.sh
+++ vnc_startup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "Starting Qemu"
-nohup qemu-system-x86_64 -name win10 -m 2048 -cpu host -vga virtio -enable-kvm -smp 2 -drive file=/root/win10.qcow2,if=virtio -net nic,model=virtio -net user -vnc :0 -full-screen -display vnc=:0  -k pt-br &
+nohup qemu-system-x86_64 -name win10 -m 2048 -cpu host -vga virtio -enable-kvm -smp 2 -drive file=/root/win10.qcow2,if=virtio -net nic,model=virtio -net user -vnc :0 -full-screen -k pt-br &
 
 
 echo "Starting noVNC"
 /opt/novnc/utils/novnc_proxy --listen 5000 --vnc localhost:5900
-
-echo "Starting websockify"
-bash
