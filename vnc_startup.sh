b838451b691f69412066ea95ad9d8fb043f54bf5
Marlon Petry <marlonpetry@gmail.com>
2023-05-24 18:51:11 -0300

    Novnc update

--- vnc_startup.sh
+++ vnc_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Qemu"
-nohup qemu-system-x86_64 -name win10 -m 2048 -cpu host -vga virtio -enable-kvm -smp 2 -drive file=/root/win10.qcow2,if=virtio -net nic,model=virtio -net user -vnc :0 -vncclient compresstcp -encodings copyrect,hextile -full-screen -display vnc=:1,1920x1080 -k pt-br &
+nohup qemu-system-x86_64 -name win10 -m 2048 -cpu host -vga virtio -enable-kvm -smp 2 -drive file=/root/win10.qcow2,if=virtio -net nic,model=virtio -net user -vnc :0 -full-screen -display vnc=:0  -k pt-br &
 
 
 echo "Starting noVNC"
