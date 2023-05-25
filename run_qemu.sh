c1644b4d8e63efc39573aa63c046c1930183fdde
jovair1994 <68813860+jovair1994@users.noreply.github.com>
2023-05-25 10:06:22 -0300

    Delete run_qemu.sh

--- run_qemu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start the VM using QEMU
-qemu-system-x86_64 -name win10 -m 2048 -cpu host -enable-kvm -smp 2 -drive file=/root/win10.qcow2,if=virtio -net nic,model=virtio -net user -soundhw hda -usb -device usb-tablet -vnc :0 -display none &
